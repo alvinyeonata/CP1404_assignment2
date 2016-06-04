a5d3db45fc7515f216457ad50bccb59f4d96e1dc
konrald <alvin.yeonata@my.jcu.edu.au>
2016-06-04 22:02:36 +0800

    saving complete

--- A2.py
+++ A2.py
@@ -9,7 +9,6 @@ class Assignment(App):
     def __init__(self):
         item_list = []
         super (Assignment,self).__init__()
-        list(item_list)
         self.item_list=item_list
 
     def build(self):
@@ -19,7 +18,9 @@ class Assignment(App):
         return self.root
 
     def items(self):
-        self.root.background_color= [1, 1, 1, 1]
+        self.item_list.clear()
+        list(self.item_list)
+        self.root.background_color= [0, 0, 1, 1]
         self.root.ids.itemsBox.clear_widgets()
         for each in self.item_list:
             button1 = Button(text=each[1])
@@ -31,24 +32,29 @@ class Assignment(App):
             self.root.ids.itemsBox.add_widget(button1)
 
     def press_key(self,instance):
-
+        print(self.item_list)
         for each in self.item_list:
             if instance.text == each[1]:
                 print(each)
-                if self.root.background_color == [1, 0, 0, 1] or self.root.background_color == [0, 1, 0, 1]:
+                if self.root.background_color == [0, 0, 1, 1]:
                     self.root.ids.label.text = "{} ({}), ${:,.2f} is {}".format(each[1], each[2], float(each[3]),each[4])
-                elif self.root.ids.hire_item.background_color == [0, 1, 0, 1]:
-                    if each[4] == 'in':
-                        self.root.ids.label.text = "Hiring: {} for ${:,.2f}".format(each[1], each[2], float(each[3]))
-                    else:
-                        self.root.ids.label.text = "Hiring: no items for $0.00"
-                elif self.root.ids.return_item.background_color == [1, 0, 0, 1]:
-                    if each[4] == 'out':
-                        self.root.ids.label.text = "Returning: {}".format(each[1])
-                    else:
-                        self.root.ids.label.text = "Returning: no items"
+
+
+    def listitem(self):
+
+        self.items()
+
+    def save(self, itemName, description, price_per_day):
+
+        add_item = "\n{},{},{},in".format(itemName, description, float(price_per_day))
+        with open("items.csv", "a") as file:
+                file.writelines(add_item)
+        self.item_list.append(add_item)
+        self.cancel()
+        self.items()
 
     def additem(self):
+
         self.root.ids.popup.open()
 
     def cancel(self):
